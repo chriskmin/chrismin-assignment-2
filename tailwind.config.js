84fa04a555c6c2fff2f84d62bbbdb55625498c00
Chris <chrismin@bu.edu>
2024-09-28 18:35:25 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensures all React files are scanned for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
