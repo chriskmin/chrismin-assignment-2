4e6bd86e5161822f390c8e93aaaa3aa147d9d305
Chris <chrismin@bu.edu>
2024-09-28 20:29:58 -0400

    react not optimal, gonna use flask and html/css

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensures all React files are scanned for Tailwind classes
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
